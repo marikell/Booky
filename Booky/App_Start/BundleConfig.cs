a783651daf48b655c533f723394605a8002152b7
marikell <marikell_silva@hotmail.com>
2018-05-06 18:03:46 -0300

    home/book css creation

--- Booky/App_Start/BundleConfig.cs
+++ Booky/App_Start/BundleConfig.cs
@@ -25,7 +25,7 @@ namespace Booky
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css", "~/Content/home.css"));
 
             bundles.Add(new StyleBundle("~/Content/images"));
 
